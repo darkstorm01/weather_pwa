22569b49f6f038909e3cdd73c3331cac5018540f
darkstorm01 <krishnamohan6364@gmail.com>
2021-10-28 12:09:29 +0530

    serviceworker registration

---
+++ public/serviceworker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "verision-1";
+const urlsToCache = ["index.html", "offline.html"];
+
+const self = this;
+
+//Install ServiceWorker
+
+self.addEventListener("install", (event) => {});
+
+//Listen for requests
+
+self.addEventListener("fetch", (event) => {});
+
+//Activate the service worker
+
+self.addEventListener("activate", (event) => {});
